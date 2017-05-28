e847e6e1fbb12494cae00070a920e045347f5b61
Wolfgang Beer <wolfgang@app-dev.io>
2017-05-28 19:09:10 +0200

    added Logger.js

---
+++ Logger.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const path = require("path");
+const env = process.env.NODE_ENV;
+const isEnvDevelopment = env === "development";
+const isEnvProduction = env === "production";
+
+class Logger {
+
+    constructor(config) {
+        this.config = config;
+        this.logPath = path.resolve(config.folder, config.file);
+        this.writeStream = null;
+    }
+
+    error(line) {
+        this.log("ERROR", line);
+    }
+
+    warn(line) {
+        this.log("WARNING", line);
+    }
+
+    info(line) {
+        this.log("INFO", line);
+    }
+
+    log(level, line) {
+
+        let logLine += `[${new Date()}] [${level}] ${line}\n`;
+
+        if (isEnvDevelopment) {
+
+            console.log(logLine);
+
+        } else if (isEnvProduction) {
+
+            if (!this.writeStream) {
+                this.writeStream = fs.createWriteStream(this.logPath, {flags: "a"});
+            }
+
+            this.writeStream.write(logLine, "utf8");
+
+        }
+
+    }
+
+}
+
+module.exports = Logger;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoDB = require("./MongoDB.js");
 const AuthProvider = require("./AuthProvider.js");
+const Logger = require("./Logger.js");
 const Service = require("./Service.js");
 
-module.exports = { Service, MongoDB, AuthProvider };
+module.exports = { Service, MongoDB, AuthProvider, Logger };
